6f87e88973a030d497654a7bd8c5bc5af10dc8f1
Hoang Trung <lhtrung307@gmail.com>
2017-10-19 11:02:05 +0700

    Refactor some old code.

--- Buoi14_JavaChat1/src/ClientChat.java
+++ Buoi14_JavaChat1/src/ClientChat.java
@@ -1,36 +1,77 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientChat {
+	private String smsServer;
+	private String smsClient;
+	private Socket sk;
+
+	public String getSmsServer() {
+		return smsServer;
+	}
+
+	public void setSmsServer(String smsServer) {
+		this.smsServer = smsServer;
+	}
+
+	public String getSmsClient() {
+		return smsClient;
+	}
+
+	public void setSmsClient(String smsClient) {
+		this.smsClient = smsClient;
+	}
+
+	public Socket getSk() {
+		return sk;
+	}
+
+	public void setSk(Socket sk) {
+		this.sk = sk;
+	}
+
+	public ClientChat(String smsServer, String smsClient, Socket sk) {
+		this.smsServer = smsServer;
+		this.smsClient = smsClient;
+		this.sk = sk;
+	}
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
 		try {
-			Socket sk = new Socket("192.168.1.106", 13579);
-			PrintWriter pw = new PrintWriter(sk.getOutputStream());
-			BufferedReader br = new BufferedReader(new InputStreamReader(sk.getInputStream(), "UTF-8"));
-			
-			while(true){
-				
-				String sms2 = br.readLine();
-				System.out.println(sms2);
-				
-				if(sms2.indexOf("exit") > -1){
-					sk.close();
+			ClientChat newClient = new ClientChat("", "", new Socket("192.168.1.106", 13579));
+			while (true) {
+				newClient.readMessage();
+				if (newClient.isExit()) {
+					newClient.sk.close();
 				}
-				
 				System.out.println("Nhập");
-				String sms = input.nextLine();
-				pw.println("Client: " + sms);
-				pw.flush();
-				
+				newClient.smsClient = input.nextLine();
+				newClient.sendMessage();
 			}
-		} catch (Exception e) {
-			// TODO: handle exception
+		} catch (IOException e) {
+			System.out.println("Have a problem when closing socket!!!");
 		}
 	}
+
+	private boolean isExit() {
+		return smsServer.indexOf("exit") > -1;
+	}
+
+	private void readMessage() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(sk.getInputStream(), "UTF-8"));
+		smsServer = br.readLine();
+		System.out.println(smsServer);
+	}
+
+	private void sendMessage() throws IOException {
+		PrintWriter pw = new PrintWriter(sk.getOutputStream());
+		pw.println("Client: " + smsClient);
+		pw.flush();
+	}
+
 }
--- Buoi14_JavaChat1/src/ServerChat.java
+++ Buoi14_JavaChat1/src/ServerChat.java
@@ -1,13 +1,8 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class ServerChat {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
 		
 		try {
 			ServerSocket ss = new ServerSocket(13579);
